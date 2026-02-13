fded4aa62f2948c6755a7de956d9e79f3d998e27
Viang41 <kathiriyavirang8@gmail.com>
2026-02-13 22:42:27 +0530

    Updated assignment 6 code - humanized content and black theme

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-// Assignment 6 - Simple Web Server with Node.js
-// Main server file
+
 
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-// port number for server
+
 const PORT = 3000;
 
-// function to read file and send response
+
 function serveFile(filePath, contentType, res) {
     fs.readFile(filePath, (err, data) => {
         if (err) {
-            // if file not found, serve 404 page
+
             serve404Page(res);
         } else {
             res.writeHead(200, { 'Content-Type': contentType });
@@ -21,12 +20,12 @@ function serveFile(filePath, contentType, res) {
     });
 }
 
-// serve 404 error page
+
 function serve404Page(res) {
     const errorPage = path.join(__dirname, 'pages', '404.html');
     fs.readFile(errorPage, (err, data) => {
         if (err) {
-            // if even 404 page not found, send plain text
+
             res.writeHead(404, { 'Content-Type': 'text/plain' });
             res.end('404 - Page Not Found!');
         } else {
@@ -36,43 +35,42 @@ function serve404Page(res) {
     });
 }
 
-// create server
+
 const server = http.createServer((req, res) => {
     let url = req.url;
-    
-    // handle routes
+
+
     if (url === '/' || url === '/home') {
-        // home page route
+
         const filePath = path.join(__dirname, 'pages', 'home.html');
         serveFile(filePath, 'text/html', res);
     }
     else if (url === '/about') {
-        // about page route
+
         const filePath = path.join(__dirname, 'pages', 'about.html');
         serveFile(filePath, 'text/html', res);
     }
     else if (url === '/contact') {
-        // contact page route  
+
         const filePath = path.join(__dirname, 'pages', 'contact.html');
         serveFile(filePath, 'text/html', res);
     }
     else if (url === '/services') {
-        // services page route (extra route)
+
         const filePath = path.join(__dirname, 'pages', 'services.html');
         serveFile(filePath, 'text/html', res);
     }
     else if (url === '/styles.css') {
-        // serve css file
+
         const filePath = path.join(__dirname, 'css', 'styles.css');
         serveFile(filePath, 'text/css', res);
     }
     else {
-        // invalid route - show 404 page
+
         serve404Page(res);
     }
 });
 
-// start listening on port
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log('Available routes:');
